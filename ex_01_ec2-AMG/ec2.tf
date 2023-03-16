8fbe795016bd921a8d19233d2d3cf1dbf2d5cfdf
amarin-alerce <amarin@alerce-group.com>
2023-03-16 20:00:24 +0100

    My LAB ex_01_ec2-AMG

---
+++ ex_01_ec2-AMG/ec2.tf
@@ -0,0 +1,21 @@
+## Create instance - will use defaults for parameters not specified (e.g. VPC, security group)
+resource "aws_instance" "server1" {
+  ami           = data.aws_ami.ubuntu_22_04_r1.id
+  instance_type = var.instance_type
+
+  subnet_id = data.aws_subnets.def_vpc_subnets_r1.ids[1]
+  tags = {
+    Name = "vm-${local.name_suffix}-1"
+  }
+}
+
+resource "aws_instance" "server2" {
+  provider = aws.region2
+  ami           = data.aws_ami.ubuntu_22_04_r2.id
+  instance_type = var.instance_type 
+
+  subnet_id = data.aws_subnets.def_vpc_subnets_r2.ids[1]
+  tags = {
+    Name = "vm-${local.name_suffix}-1"
+  }
+}
---
+++ ex_01_ec2-AMG/outputs.tf
@@ -0,0 +1,28 @@
+output "public_ip1" {
+  description = "Public IP of instance"
+  value       = aws_instance.server1.public_ip
+}
+output "public_ip2" {
+  description = "Public IP of instance"
+  value       = aws_instance.server2.public_ip
+}
+output "ami1" {
+  description = "AMI of instance"
+  value       = aws_instance.server1.ami
+}
+output "ami2" {
+  description = "AMI of instance"
+  value       = aws_instance.server2.ami
+}
+data "aws_caller_identity" "current" {
+}
+output "user_identity" {
+  description = "My identity"
+  value       = data.aws_caller_identity.current
+}
+output "az_subnet_r1" {
+  value = data.aws_availability_zones.azs_available_r1[*].names[1]
+}
+output "az_subnet_r2" {
+  value = data.aws_availability_zones.azs_available_r2[*].names[1]
+}
