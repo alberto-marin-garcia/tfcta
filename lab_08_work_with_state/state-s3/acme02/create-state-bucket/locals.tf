6915e2d9f330833df022ad39c8966ea7aedc3324
amarin-alerce <amarin@alerce-group.com>
2023-03-22 19:46:42 +0100

    Reorganize name folders

---
+++ lab_08_work_with_state/state-s3/acme02/create-state-bucket/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  ## for troubleshooting
+  account_id = data.aws_caller_identity.current.account_id
+}
--- lab_08_work_with_state/state-s3/acme02/create-state-bucket/s3.tf
+++ lab_08_work_with_state/state-s3/acme02/create-state-bucket/s3.tf
@@ -1,8 +1,3 @@
-locals {
-  ## for troubleshooting
-  account_id = data.aws_caller_identity.current.account_id
-}
-
 ## Note we use the account ID to make sure the buckets are unique worldwide
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "${var.project}-terraform-state-${local.account_id}-${var.environment}"
