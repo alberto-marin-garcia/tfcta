44d771db2ec7b051f5c2a63ccc5c4047b0464517
amarin-alerce <amarin@alerce-group.com>
2023-03-29 19:11:33 +0200

    Empty ouputs added

--- lab_09_modules/02_creating_modules/lab_09b_create/WayneCorp_github/project3/main.tf
+++ lab_09_modules/02_creating_modules/lab_09b_create/WayneCorp_github/project3/main.tf
@@ -1,4 +1,4 @@
-module "s3_proj2" {
+module "s3_bucket" {
   source            = "github.com/alberto-marin-garcia/tfcourse-modules//s3-modules?ref=1.2"
   bucket_name = "${var.lab_number}-${var.bucket_name}"
   versioning = var.versioning
